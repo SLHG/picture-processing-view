d08215dea92d360c0144e2359e67e047f24223a7
shazhibin <shazhibin07@163.com>
2021-09-06 10:25:08 +0800

    添加图片移动缩放

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -9,6 +9,7 @@ Page({
         primarySize: 'default',
         warnSize: 'default',
         disabled: false,
+        isLoading: false,
         plain: false,
         loading: false,
         image1Src: '',
@@ -16,8 +17,11 @@ Page({
         context: null,
         x: 0,
         y: 0,
-        dw: 200,
-        dh: 200
+        dw: 100,
+        dh: 100,
+        image1: null,
+        mvTid: null,
+        chgTid: null
     },
 
     /**
@@ -31,22 +35,20 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        const query = wx.createSelectorQuery()
-        query.select('#canvas1')
-            .fields({
-                node: true,
-                size: true
+        const query = wx.createSelectorQuery().in(this)
+        query.select('#canvas1').fields({
+            node: true,
+            size: true
+        }).exec((res) => {
+            const canvas = res[0].node
+            const ctx = canvas.getContext('2d')
+            canvas.width = this.data.dw
+            canvas.height = this.data.dh
+            this.setData({
+                canvas: canvas,
+                context: ctx
             })
-            .exec((res) => {
-                const canvas = res[0].node
-                canvas.width = this.data.dw
-                canvas.height = this.data.dh
-                const ctx = canvas.getContext('2d')
-                this.setData({
-                    canvas: canvas,
-                    context: ctx
-                })
-            })
+        })
     },
 
     /**
@@ -97,6 +99,9 @@ Page({
             sizeType: ['original', 'compressed'],
             sourceType: ['album', 'camera'],
             success(res) {
+                t.setData({
+                    isLoading: true
+                })
                 // tempFilePath可以作为img标签的src属性显示图片
                 const tempFilePaths = res.tempFilePaths
                 wx.uploadFile({
@@ -106,9 +111,10 @@ Page({
                     success(res) {
                         let data = JSON.parse(res.data)
                         t.setData({
-                            image1Src: 'data:image/png;base64,' + data.result
+                            image1Src: 'data:image/png;base64,' + data.result,
+                            isLoading: false
                         })
-                        t.data.context.fillStyle = 'white';
+                        t.data.context.fillStyle = "white";
                         t.drawImage();
                     }
                 })
@@ -121,12 +127,60 @@ Page({
         this.drawImage();
     },
     drawImage: function () {
-        let canvas = this.data.canvas;
-        // 创建一个图片
+        let data = this.data
+        let canvas = data.canvas;
+        let context = data.context;
+        // 创建一个图片 
         const image = canvas.createImage()
-        image.src = this.data.image1Src // 要加载的图片 url
-        let context = this.data.context;
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(image, this.data.x, this.data.y, this.data.dw, this.data.dh);
+        image.src = data.image1Src // 要加载的图片 url
+        image.width = data.dw
+        image.height = data.dh
+        image.onload = function () {
+            console.log("图片加载完成")
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(image, data.x, data.y, data.dw, data.dh);
+        }
+        image.onerror = function () {
+            console.log("图片加载失败")
+        }
+    },
+    btnStartMovePhoto: function (event) {
+        let dataThis = this
+        let a = parseInt(event.target.dataset.a)
+        let b = parseInt(event.target.dataset.b)
+        let mvTid = setInterval(function () {
+            let x = dataThis.data.x + a
+            let y = dataThis.data.y + b
+            dataThis.setData({
+                x: x,
+                y: y
+            })
+            dataThis.drawImage();
+        }, 50)
+        this.setData({
+            mvTid: mvTid
+        })
+    },
+    btnEndMovePhoto: function () {
+        clearInterval(this.data.mvTid);
+    },
+    btnStartChangePhoto: function (event) {
+        let dataThis = this
+        let a = parseInt(event.target.dataset.a)
+        let chgTid = setInterval(function () {
+            let dw = dataThis.data.dw + a;
+            let dh = dataThis.data.dh + a
+            dataThis.setData({
+                dw: dw,
+                dh: dh
+            })
+            dataThis.drawImage();
+        }, 50)
+        this.setData({
+            chgTid: chgTid
+        })
+    },
+    BtnEndChangePhoto: function () {
+        clearInterval(this.data.chgTid);
     }
 })
